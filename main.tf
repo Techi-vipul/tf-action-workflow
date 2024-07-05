89c025da008ea72e085f540f583760c2a970ad02
Vipul chauhan <82360686+Techi-vipul@users.noreply.github.com>
2024-07-05 14:51:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,16 @@
 terraform {
-   required_version = "~> 1.8.5"
+  required_version = "~> 1.8.3"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.53.0"
+      version = "5.57.0"
+    }
+  }
+  cloud {
+    organization = "Terraform-June-Learning"
+
+    workspaces {
+      name = "Cli-driven"
     }
   }
 }
@@ -14,21 +21,20 @@ provider "aws" {
 }
 
 // Call a Terraform Cloud Module
-
-module "tfmodule" {
-  count = 2
-  source  = "app.terraform.io/Terraform-June-Learning/tfmodule/aws"
-  version = "1.0.0"
+module "module1" {
+  count    = 2
+  source   = "app.terraform.io/Terraform-June-Learning/tfmodule/aws"
+  version  = "1.0.0"
   ec2_size = "t2.micro"
   ec2_name = { Name = "server${count.index}", env = "dev" }
 }
 // for printing output
 output "public_ip" {
-  value = module.tfmodule[0].ec2_public_ip
+  value = module.module1[0].ec2_public_ip
 }
 output "private_ip" {
-  value = module.tfmodule[0].ec2_private_ip
+  value = module.module1[0].ec2_private_ip
 }
 output "dns_name" {
-  value = module.tfmodule[0].ec2_public_dns
+  value = module.module1[0].ec2_public_dns
 }
