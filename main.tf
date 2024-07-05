071610e6add527f21281d13467efed90c90c5720
Vipul chauhan <82360686+Techi-vipul@users.noreply.github.com>
2024-07-05 13:10:14 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,34 @@
+terraform {
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.53.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+// Create a Local Module
+
+module "myec2mdule" {
+  count = 2
+  source  = "app.terraform.io/Terraform-June-Learning/tfmodule/aws"
+  version = "1.0.0"
+  ec2_size = "t2.micro"
+  ec2_name = { Name = "server${count.index}", env = "dev" }
+}
+// for printing output
+output "public_ip" {
+  value = module.myec2mdule[0].ec2_public_ip
+}
+output "private_ip" {
+  value = module.myec2mdule[0].ec2_private_ip
+}
+output "dns_name" {
+  value = module.myec2mdule[0].ec2_public_dns
+}
